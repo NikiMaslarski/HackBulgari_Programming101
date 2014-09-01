183f0d07773d74704536a2c647359cb2f1593058
NikiMaslarski <jjniki93@gmail.com>
2014-09-02 01:33:47 +0300

    unfinished solution to DashInsert

---
+++ DashInsert/solution.py
@@ -0,0 +1,10 @@
+def Dash_insert(num):
+    list_of_numbers = [int(i) for i in str(num)]
+    result = []
+    for i in range(len(list_of_numbers) - 1):
+        if number[i] % 2 == 1 && number[i+1] % 2:
+            
+
+
+
+Dash_insert(99946)
