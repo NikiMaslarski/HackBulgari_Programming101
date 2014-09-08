78a42b044c005ca6443cb2da97bef50524b140f8
NikiMaslarski <jjniki93@gmail.com>
2014-09-08 19:13:20 +0300

    not working

--- DashInsert/solution.py
+++ DashInsert/solution.py
@@ -1,9 +1,10 @@
 def Dash_insert(num):
-    list_of_numbers = [int(i) for i in str(num)]
+    list_of_numbers = [int(digit) for digit in str(num)]
     result = []
     for i in range(len(list_of_numbers) - 1):
-        if number[i] % 2 == 1 && number[i+1] % 2:
-            
+        result.append(number[i])
+        if number[i] % 2 == 1  && number[i+1] % 2 == 1:
+
 
 
 
