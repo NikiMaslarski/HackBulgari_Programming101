e9e162aaa00b709d1baffde6c599447f59524a88
NikiMaslarski <jjniki93@gmail.com>
2014-12-04 17:08:56 +0200

    uh

--- Exam/Tic-tac-toe/game.py
+++ Exam/Tic-tac-toe/game.py
@@ -1,6 +1,12 @@
 class Game:
 
+    WIN_LINES = ([])
+
     def __init__(self):
         self._board = 3 * [3 * [0]]
 
-    def 
+    def make_turn(self, player_symbol, x, y):
+        self._board[x][y] = player_symbol
+
+    def check_for_end_game(self):
+        
